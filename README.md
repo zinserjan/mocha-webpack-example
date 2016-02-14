456552af6059b114238f258442eab7a98771210a
Jan-André Zinser <zinserjan@gmail.com>
2016-02-14 23:41:50 +0100

    include packages via npm

--- README.md
+++ README.md
@@ -4,10 +4,8 @@
 
 ### setup
 
-1. checkout git repo [mocha-webpack] and run 'npm install', 'npm run build' && 'npm link'
-2. run here 'npm link mocha-webpack'
-3. look into package.json for scripts
-  - e.g. 'npm run test'
+1. npm install
+2. npm run test
 
 
 
--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,6 +1,22 @@
 var path = require('path');
-var nodeExternals = require('webpack-node-externals');
 var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var WebpackInfoPlugin = require('webpack-info-plugin');
+
+var info = new WebpackInfoPlugin({
+  stats: {
+    version: false,
+    colors: true,
+    hash: false,
+    timings: true,
+    assets: false,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  },
+  state: true
+});
+
 
 
 module.exports = {
@@ -14,8 +30,8 @@ module.exports = {
     devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
   },
   plugins: [
-    new webpack.BannerPlugin('require("source-map-support").install();',
-        { raw: true, entryOnly: false })
+    new webpack.BannerPlugin('require("source-map-support").install();', { raw: true, entryOnly: false }),
+    info
   ],
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
