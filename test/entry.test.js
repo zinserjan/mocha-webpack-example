b87fa826011eaf21e519c20f421349e9b98f53e0
Jan-André Zinser <zinserjan@gmail.com>
2017-01-10 14:18:35 +0100

    jquery test

--- test/entry.test.js
+++ test/entry.test.js
@@ -1,12 +1,18 @@
 // import '../src/entry';
+import assert from 'assert';
+import $ from 'jquery';
 
-
-window.console.log("lallaa");
-window.console.log("ffff");
 
 describe('entry', function () {
 
   it('just works', function () {
     console.log('works');
+  });
+
+  it('jquery works', function () {
+    const text = `That is a very long text...`;
+    const html = `<div>${text}</div>`;
+
+    assert.strictEqual($(html).text(), text);
   });
 });
